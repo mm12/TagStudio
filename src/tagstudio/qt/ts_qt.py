3dacdbac780ec8394f2425da90db6fbfbba71180
TheBobBobs <84781603+TheBobBobs@users.noreply.github.com>
2026-01-23 06:45:36 +0000

    fix: persist entry selection across pages and save scroll positions (#1248)
    * fix: persist entry selection across pages and save scroll positions
    
    * fix: add badges to all selected entries not just visible ones

--- src/tagstudio/qt/ts_qt.py
+++ src/tagstudio/qt/ts_qt.py
@@ -17,6 +17,7 @@ import sys
 import time
 from argparse import Namespace
 from collections import OrderedDict
+from collections import OrderedDict
 from functools import partial
 from pathlib import Path
 from queue import Queue
